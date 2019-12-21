3ec8edd436105a92ad0a4340a9e4e453672f3213
Justin Phillips <justinphil02@yahoo.com>
2019-12-21 23:36:50 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -62,7 +62,7 @@ end
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by 
   # release date descending
-  a = Movie.order("release_date DESC")
+  a = Movie.where("release_date < ?", 2002)
   a.delete_by("release_date < ?", 2002)
   a
 end
